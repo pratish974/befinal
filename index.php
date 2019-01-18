30e722104b72d11cdfd869256aa1968d3522fbc2
Pratish Chavan <chavanpratish@live.com>
2019-01-18 21:44:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,91 @@
 <?php
  require 'account\core.inc.php';
  require 'account\connect.inc.php';
- 
+ include 'sand\phpfsplit.php';
+
  if (loggedin()) {
   //catches firstname and surname with getuserfield function in core.inc.php
   $firstname = getuserfield('fname');
   $surname = getuserfield('lname');
   echo 'You\'re logged in, '.$firstname.' '.$surname.'. <a href="account\logout.php">Log out</a><br>';
-  }else {
+  
+    
+
+
+if(isset($_FILES['image'])){
+    $errors= array();
+    $file_name = $_FILES['image']['name']; 
+    $file_size =$_FILES['image']['size'];
+	
+	 function formatSizeUnits($file_size) // size conversion
+    {
+        if ($file_size >= 1073741824)
+        {
+            $file_size = number_format($file_size / 1073741824, 2);
+        }
+        elseif ($file_size >= 1048576)
+        {
+            $file_size = number_format($file_size / 1048576, 2);
+        }
+        elseif ($file_size >= 1024)
+        {
+            $file_size = number_format($file_size / 1024, 2);
+        }
+        elseif ($file_size > 1)
+        {
+            $file_size = $file_size;
+        }
+        elseif ($file_size == 1)
+        {
+            $file_size = $file_size;
+        }
+        else
+        {
+            $file_size = '0 bytes';
+        }
+
+        return ceil($file_size);
+}
+
+
+    $file_tmp =$_FILES['image']['tmp_name'];
+    $file_type=$_FILES['image']['type'];
+    
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+	  
+$tmp_size = formatSizeUnits($file_size);
+
+if(empty($errors)==true){
+		  $parts = fsplit($file_name,$tmp_size); //filename with extenstion and size should be given
+         //move_uploaded_file($file_tmp,"splits/".$file_name);
+         echo "<br> Success";
+      }else{
+         print_r($errors);
+      } 
+	  
+}
+   
+   echo '
+   
+   <html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
+   
+   ';
+ 
+ }
+  else {
   include 'account\login.php';
  }
 ?>
