f84c59e6020cec657d42ed40a4d729f9b7f71c26
Pratish Chavan <chavanpratish@live.com>
2019-01-18 21:36:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
- require 'core.inc.php';
- require 'connect.inc.php';
+ require 'account\core.inc.php';
+ require 'account\connect.inc.php';
  
  if (loggedin()) {
   //catches firstname and surname with getuserfield function in core.inc.php
   $firstname = getuserfield('fname');
   $surname = getuserfield('lname');
-  echo 'You\'re logged in, '.$firstname.' '.$surname.'. <a href="logout.php">Log out</a><br>';
+  echo 'You\'re logged in, '.$firstname.' '.$surname.'. <a href="account\logout.php">Log out</a><br>';
   }else {
-  include 'login.php';
+  include 'account\login.php';
  }
 ?>
